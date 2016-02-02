962751492ff704965494b4ad2b61e4e9a1eb9768
Ryan Bradley <bradleyrp@gmail.com>
2016-02-02 16:31:03 -0500

    corrected cuda flag in protein_atomistic procedure

--- amx/procedures/protein_atomistic.py
+++ amx/procedures/protein_atomistic.py
@@ -198,8 +198,8 @@ def trim_waters(structure='solvate-dense',gro='solvate',gap=3,boxvecs=None):
 	with open(wordspace['step']+'script-vmd-trim.tcl','w') as fp:
 		for line in vmdtrim: fp.write(line+'\n')
 	vmdlog = open(wordspace['step']+'log-script-vmd-trim','w')
-	os.environ['VMDNOCUDA'] = "1"	
-	p = subprocess.Popen(gmxpaths['vmd']+' -dispdev text -e script-vmd-trim.tcl',
+	#---previously used os.environ['VMDNOCUDA'] = "1" but this was causing segfaults on green
+	p = subprocess.Popen('VMDNOCUDA=1 '+gmxpaths['vmd']+' -dispdev text -e script-vmd-trim.tcl',
 		stdout=vmdlog,stderr=vmdlog,cwd=wordspace['step'],shell=True)
 	p.communicate()
 	with open(wordspace['bash_log'],'a') as fp:
