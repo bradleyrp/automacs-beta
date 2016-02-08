44d006d34a72af33a495e79bc612d18832220ac2
Ryan Bradley <bradleyrp@gmail.com>
2016-02-08 18:04:07 -0500

    start function copies sources and files

--- amx/base/functions.py
+++ amx/base/functions.py
@@ -151,11 +151,15 @@ def start(name):
 	#---files keyword in the settings block refers to files that should be copied from inputs
 	if 'files' in wordspace:
 		fns = eval(wordspace['files'])
-		for fn in fns: filecopy('inputs/'+fn,wordspace['step']+fn)
+		for fn in fns: 
+			try: filecopy('inputs/'+fn,wordspace['step']+fn)
+			except: continue
 	#---sources keyword in the settings block refers to directories that should be copied from inputs
 	if 'sources' in wordspace:
 		source_dirs = eval(wordspace['sources'])
-		for dn in source_dirs: dircopy('inputs/'+dn,wordspace['step']+dn)
+		for dn in source_dirs: 
+			try: dircopy('inputs/'+dn,wordspace['step']+dn)
+			except: continue
 
 @narrate
 def filecopy(src,dest):
