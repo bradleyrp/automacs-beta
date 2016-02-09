cf2d613e3fd6a539e8d09b3e63ca4efd1674bdd9
Ryan Bradley <bradleyrp@gmail.com>
2016-02-09 10:52:25 -0500

    toc for new procedures

---
+++ amx/procedures/toc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python 
+
+"""
+Table of contents for available procedures. 
+Note that this should be implemented more carefully with a dictionary which is read by amx/__init__.py
+"""
+
+procedure_toc = {
+	'aamd,protein':'protein_atomistic',
+	'cgmd,bilayer':'cgmd_bilayer',
+	'cgmd,protein':'cgmd_protein',
+	}
+
+if 0:
+	if procedure == 'aamd,protein':
+		#---imports: atomistic protein in water
+		from procedures.protein_atomistic import *
+		wordspace['command_library'] = interpret_command(command_library)
+		wordspace['mdp_specs'] = mdp_specs
+	elif procedure == 'cgmd,bilayer':
+		#---imports: coarse-grained bilayer in water
+		from procedures.cgmd_bilayer import *
+		wordspace['command_library'] = interpret_command(command_library)
+		wordspace['mdp_specs'] = mdp_specs
+	elif procedure == 'cgmd,protein':
+		#---imports: coarse-grained protein in water
+		from procedures.cgmd_protein import *
+	else: raise Exception('[ERROR] unclear procedure %s'%procedure)
+
