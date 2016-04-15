6b1928118c707fd95218132750ed0f4c0f94d5c4
Ryan Bradley <bradleyrp@gmail.com>
2016-04-14 23:46:21 -0400

    typo

--- amx/base/journal.py
+++ amx/base/journal.py
@@ -7,7 +7,7 @@ from functools import wraps
 #---always print to stdout without a buffer
 unbuffered = os.fdopen(sys.stdout.fileno(),'w',0)
 #---! the following was painful. fails on OSX without this.
-os.dup2(unbuffered,sys.stderr.fileno())
+# os.dup2(unbuffered,sys.stderr.fileno())
 
 def report(text,tag='status',newline=False,newline_trail=False,watch_file=None):
 
